58f29ce6dad0cb361f820bb0023aeb8de2e8d825
harsh2011 <harsh20111997@gmail.com>
2017-03-03 00:38:56 +0530

    Add files via upload

--- HidingText(LSB)/Level1/SG.py
+++ HidingText(LSB)/Level1/SG.py
@@ -10,9 +10,23 @@ from binary2string import imagetobinarytochartostring
 img = cv2.imread("Image\\Untitled.png")
 img = cv2.cvtColor(img,cv2.COLOR_RGB2RGBA)
 
+width = len(img)
+heigth = len(img)
 
+min = 0
 
-d1 =  stringtochartobinarytoimage("harsh"+chr(3))
+if(width<heigth):
+    min = width-3
+    min = int(min * min)
+else:
+    min = heigth-3
+    min = int(min * min)
+
+print("minimum character "+str(min))
+
+str = str(input("Enter string to hide: "))
+
+d1 =  stringtochartobinarytoimage(str+chr(3))
 print(d1)
 d2 = d1arraytod2array(d1)
 print(d2)
@@ -23,9 +37,5 @@ decode = timetodecode(encodedimg,key)
 print(decode)
 str = imagetobinarytochartostring(decode)
 print(str)
-# key = timetoencode(img,)
-# print(key)
-#
-# encodedimg = cv2.imread("encode.png",cv2.IMREAD_UNCHANGED)
-# timetodecode(encodedimg,key)
+
 
